cba7cc64ebe79bbeb0a70bad95ce499d6d42adb5
ritigit7 <ritikm01092003@gmail.com>
2024-01-25 18:06:57 +0530

    remove unwanted library

--- src/L_1457_Pseudo_Palindromic.java
+++ src/L_1457_Pseudo_Palindromic.java
@@ -1,9 +1,5 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class L_1457_Pseudo_Palindromic {
     public static void main(String[] args) {
