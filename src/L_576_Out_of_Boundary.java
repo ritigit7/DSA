1fcfd6a9606943a8bf3428640c29f6e96f31b0b7
ritigit7 <ritikm01092003@gmail.com>
2024-01-26 22:13:25 +0530

    final

--- src/L_576_Out_of_Boundary.java
+++ src/L_576_Out_of_Boundary.java
@@ -1,49 +1,47 @@
 public class L_576_Out_of_Boundary {
     static int ans = 0;
+    static int temp = 0;
 
     public static void main(String[] args) {
-        System.out.println("Final:" + findPaths(2, 2, 2, 0, 0));
-        // System.out.println("Final:" + findPaths(1, 3, 3, 0, 1));
+        // System.out.println("Final:" + findPaths(1, 3, 2, 0, 0));
+        // System.out.println("Final:" + findPaths(2, 2, 2, 0, 0));
+        System.out.println("Final:" + findPaths(1, 3, 3, 0, 1));
     }
 
     static int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         for (int i = 1; i <= maxMove; i++) {
+            ans = ans - temp;
             fn(m, n, i, startRow, startColumn);
+            temp = ans;
         }
         return ans;
     }
 
-    // 1,-1,0
-    // 1,0,-1
-    // 1,-2,-1
-    // 2,2,0
-    // 1,-1,-2
-    // 1,-1,2
     private static void fn(int m, int n, int i, int startRow, int startColumn) {
         if (i > 0) {
-            if (startRow - i == -1) {
+            if (startRow - 1 == -1) {
                 ans++;
-                System.out.println(i + "," + (startRow - i) + "," + startColumn);
+                System.out.println(i + "," + (startRow - 1) + "," + startColumn);
             } else {
-                fn(m, n, i - 1, startRow - i, startColumn);
+                fn(m, n, i - 1, startRow - 1, startColumn);
             }
-            if (startRow + i == m) {
+            if (startRow + 1 == m) {
                 ans++;
-                System.out.println(i + "," + (startRow + i) + "," + startColumn);
+                System.out.println(i + "," + (startRow + 1) + "," + startColumn);
             } else {
-                fn(m, n, i - 1, startRow + i, startColumn);
+                fn(m, n, i - 1, startRow + 1, startColumn);
             }
-            if (startColumn - i == -1) {
+            if (startColumn - 1 == -1) {
                 ans++;
-                System.out.println(i + "," + startRow + "," + (startColumn - i));
+                System.out.println(i + "," + startRow + "," + (startColumn - 1));
             } else {
-                fn(m, n, i - 1, startRow, startColumn - i);
+                fn(m, n, i - 1, startRow, startColumn - 1);
             }
-            if (startColumn - i == n) {
+            if (startColumn + 1 == n) {
                 ans++;
-                System.out.println(i + "," + startRow + "," + (startColumn + i));
+                System.out.println(i + "," + startRow + "," + (startColumn + 1));
             } else {
-                fn(m, n, i - 1, startRow, startColumn + i);
+                fn(m, n, i - 1, startRow, startColumn + 1);
             }
         }
         return;
