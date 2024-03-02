13d1329d420b006b79d47941be108fd2a4c9f4d5
ritigit7 <ritikm01092003@gmail.com>
2024-03-02 11:41:34 +0530

    t

---
+++ src/L_543_Diameter_of_Binary.java
@@ -0,0 +1,24 @@
+public class L_543_Diameter_of_Binary {
+    class Solution {
+        public int diameterOfBinaryTree(TreeNode root) {
+            maxDepth(root);
+            return ans;
+        }
+
+        private int ans = 0;
+
+        int maxDepth(TreeNode root) {
+            if (root == null)
+                return 0;
+
+            final int l = maxDepth(root.left);
+            final int r = maxDepth(root.right);
+            ans = Math.max(ans, l + r);
+            return 1 + Math.max(l, r);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
